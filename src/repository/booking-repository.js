c529cdc287cc32d5d6cdbc6d27134c3aea5261a8
Sachin-8125 <devinewolf7@gmail.com>
2024-08-21 18:35:14 +0530

    Added craete Booking repository code

---
+++ src/repository/booking-repository.js
@@ -0,0 +1,31 @@
+const {StatusCodes} = require('http-status-codes');
+const {Booking} = require('../models/index');
+const { AppError,ValidationError } = require('../utils/errors/index');
+
+
+class BookingRepository{
+    async create(data){
+        try {
+            const booking = await Booking.create(data);
+            return booking;
+        } catch (error) {
+            if(error.name=='SequelizeValidationError'){
+                throw new ValidationError(error);
+            }
+            throw new AppError(
+                'RepositoryError',
+                'Cannot create Booking',
+                'There is some issue in creating the Booking, please try again later',
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    async upadte(data){
+        
+    }
+
+
+}
+
+module.exports = BookingRepository;
